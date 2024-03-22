6eebcbed0996fd5803a59b9c83b1169274ac9214
wukongdaily <2666180@gmail.com>
2024-03-22 15:35:43 +0800

    test

---
+++ tmp/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+backup_to_nas() {
+    local model=$1
+    pw=$2
+    mkdir -p /mnt/nas/$model
+    opkg update
+    echo "安装 Cifs 用于挂载NAS空间"
+    opkg install kmod-fs-cifs cifsmount
+    mount -t cifs //192.168.66.237/IntelSSD /mnt/nas -o username=wukong,password=$pw,iocharset=utf8
+    cd /mnt/nas/$model
+    echo "备份overlay"
+    tar czvf overlay-backup.tar.gz /overlay
+    echo "备份已安装列表"
+    opkg list-installed >packages-list.txt
+    echo "备份OPKG配置"
+    wget https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/$model/distfeeds.conf
+    echo "拷贝恢复脚本"
+    cp /mnt/nas/mt2500/recovery recovery
+    ls
+}
+
+backup_to_nas
