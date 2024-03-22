14b22c71672882ba7ca4c819bab5a0b5592514a8
wukongdaily <2666180@gmail.com>
2024-03-22 17:41:48 +0800

    Update backup.sh

--- tmp/backup.sh
+++ tmp/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# wget -O backup.sh https://raw.githubusercontent.com/wukongdaily/tempshell/master/tmp/backup.sh && chmod +x backup.sh 
+# wget -O backup.sh https://raw.githubusercontent.com/wukongdaily/tempshell/master/tmp/backup.sh && chmod +x backup.sh
 # sh backup.sh mt-6000 xxxxx
 # 检查是否传入了两个参数
 if [ "$#" -ne 2 ]; then
@@ -10,7 +10,6 @@ fi
 model=$1
 pw=$2
 backup_to_nas() {
-    
     mkdir -p /mnt/nas
     opkg update
     echo "安装 Cifs 用于挂载NAS空间"
@@ -24,7 +23,26 @@ backup_to_nas() {
     echo "备份已安装列表"
     opkg list-installed >packages-list.txt
     echo "备份OPKG配置"
-    wget https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/$model/distfeeds.conf
+
+    if [ "$model" = "mt-6000" ]; then
+        kernel_version=$(uname -r)
+        echo "$model kernel version: $kernel_version"
+        case $kernel_version in
+        5.4*)
+            mt6000_opkg="https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/mt-6000/distfeeds-5.4.conf"
+            ;;
+        5.15*)
+            mt6000_opkg="https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/mt-6000/distfeeds.conf"
+            ;;
+        *)
+            echo "Unsupported kernel version: $kernel_version"
+            return 1
+            ;;
+        esac
+        wget ${mt6000_opkg}
+    else
+        wget https://raw.githubusercontent.com/wukongdaily/gl-inet-onescript/master/$model/distfeeds.conf
+    fi
     echo "拷贝恢复脚本"
     cp /mnt/nas/mt2500/recovery recovery
     ls
