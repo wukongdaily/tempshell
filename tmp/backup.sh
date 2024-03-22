fb2132eb04b4d8ea9369846960c2349ff36c5292
wukongdaily <2666180@gmail.com>
2024-03-22 17:26:32 +0800

    Update backup.sh

--- tmp/backup.sh
+++ tmp/backup.sh
@@ -5,11 +5,13 @@ model=$1
 pw=$2
 backup_to_nas() {
     
-    mkdir -p /mnt/nas/$model
+    mkdir -p /mnt/nas
     opkg update
     echo "安装 Cifs 用于挂载NAS空间"
     opkg install kmod-fs-cifs cifsmount
     mount -t cifs //192.168.66.237/IntelSSD /mnt/nas -o username=wukong,password=$pw,iocharset=utf8
+    cd /mnt/nas
+    mkdir -p $model
     cd /mnt/nas/$model
     echo "备份overlay"
     tar czvf overlay-backup.tar.gz /overlay
